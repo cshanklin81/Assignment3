f580076a51673ef87721422ec890912ade1fa14e
cshanklin81 <71786374+cshanklin81@users.noreply.github.com>
2021-02-03 22:45:42 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Assignment3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Movies()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Movies(Movies movieresponse)
+        {
+            MovieStorage.addMovie(movieresponse);
+            return View("Confirmation", movieresponse);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MyPodcasts()
+        {
+            return View();
+        }
+
+        public IActionResult CurrentMovies()
+        {
+            return View(MovieStorage.Movies.Where(m => m.Title != "Independence Day"));
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MovieStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3.Models
+{
+    public class MovieStorage
+    {
+        private static List<Movies> newMovies = new List<Movies>();
+
+        public static IEnumerable<Movies> Movies => newMovies;
+
+        public static void addMovie(Movies movie)
+        {
+            newMovies.Add(movie);
+        }
+    }
+}
